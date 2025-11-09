a20cb6f40ee2e939c56dfccbd771eb419877ef0b
[dyad] <git@dyad.sh>
2025-11-09 18:54:27 +0530

    [dyad] Fixed theme provider error for toasts. - wrote 4 file(s), added next-themes package(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,12 @@
 import { createRoot } from "react-dom/client";
 import App from "./App.tsx";
 import "./globals.css";
+import { ThemeProvider } from "./components/theme-provider.tsx";
 
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider defaultTheme="light" storageKey="vite-ui-theme">
+      <App />
+    </ThemeProvider>
+  </React.StrictMode>
+);
